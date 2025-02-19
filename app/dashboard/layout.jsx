8ee71782913c1f5db3c4e7b501e01a8f44499276
Steelgrou <steelgrou03@gmail.com>
2025-02-19 11:23:32 +0500

    add dashboard and login page

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+// import Sidebar from '../../ui/dashboard/sidebar/sidebar';
+// import Navbar from '../../ui/dashboard/navbar/navbar';
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            {/* <div><Sidebar /></div> */}
+            <div>{children}</div>
+        
+        </div>
+    )
+}
