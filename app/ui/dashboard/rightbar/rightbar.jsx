df682cf5c0f16fcd758ebc12feb215f5ba85a373
Steelgrou <steelgrou03@gmail.com>
2025-03-06 20:13:06 +0500

    finish dashboard

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,38 @@
-
+import {MdReadMore } from "react-icons/md"
+import styles from "./rightbar.module.css"
 
 export default function Rightbar() {
   return (
-    <div>Rightbar</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <img style={{width:"100px"}} src="/profil.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>How  to use the new version of the admin dashboard</h3>
+          <span className={styles.subtitle} >Takes 4 minutes to learn</span>
+          <p className={styles.desc}> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+          <button className={styles.button}>
+            <MdReadMore/> Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <img style={{width:"100px"}} src="/profil.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>How  to use the new version of the admin dashboard</h3>
+          <span className={styles.subtitle} >Takes 4 minutes to learn</span>
+          <p className={styles.desc}> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+          <button className={styles.button}>
+            <MdReadMore/> Watch
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
